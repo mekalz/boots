4391e518bd5c5700599f6b931b5d11df77e74893
mekalz <mekal.zheng@gmail.com>
2016-04-22 14:27:11 +0800

    add script for installing vim configuration(spf13-vim)

---
+++ vim/install.sh
@@ -0,0 +1,18 @@
+# use repo: https://github.com/spf13/spf13-vim
+# Requires Git 1.7+ and Vim 7.3+
+
+# bakeup old vimrcs
+NOW=$(date +"%Y-%m-%d-%H-%M")
+BAKDIR=~/vimrc.bakup-$NOW
+
+vimrc_count=$(ls -a | grep .vimrc | wc -l)
+echo $vimrc_count > 0
+if [ $vimrc_count -gt 0 ]; then
+  echo "make a bakup for old vimrc files in $BAKDIR ..."
+  mkdir -p BAKDIR
+  mv -f ~/.vimrc* BAKDIR 
+fi
+
+# install spf13-vim3 from remote
+echo "install spf13-vim3 ..."
+sh <(curl https://j.mp/spf13-vim3 -L)
