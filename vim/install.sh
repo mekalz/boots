1ce792113645c169b65e465c4bb46ed904f96bef
mekalz <mekal.zheng@gmail.com>
2016-04-25 07:52:56 +0800

    use bash to exec install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,9 @@
+#! /bin/bash
+
 # use repo: https://github.com/spf13/spf13-vim
 # Requires Git 1.7+ and Vim 7.3+
+
+set -u
 
 # bakeup old vimrcs
 NOW=$(date +"%Y-%m-%d-%H-%M")
@@ -10,9 +14,9 @@ echo $vimrc_count > 0
 if [ $vimrc_count -gt 0 ]; then
   echo "make a bakup for old vimrc files in $BAKDIR ..."
   mkdir -p BAKDIR
-  mv -f ~/.vimrc* BAKDIR 
+  mv -f ~/.vimrc* BAKDIR
 fi
 
 # install spf13-vim3 from remote
 echo "install spf13-vim3 ..."
-sh <(curl https://j.mp/spf13-vim3 -L)
+bash <(curl https://j.mp/spf13-vim3 -L)
